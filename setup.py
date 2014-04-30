8b991e94351580b4d7747eacb404780c264ce31e
Tom Leach <tom@gc.io>
2014-04-30 17:04:57 -0400

    Bump version to 0.2.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="Schemer",
-    version="0.2.2",
+    version="0.2.3",
     author="Tom Leach",
     author_email="tom@gc.io",
     description="Powerful schema-based validation of Python dicts",
