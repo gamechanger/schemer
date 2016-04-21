1d523d5787cbf058d6877044292016b9403619ba
Jason Morganson <jason@morganson.me>
2016-04-21 12:15:51 -0400

    chore(version): Bump version to 0.2.10

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="Schemer",
-    version="0.2.9",
+    version="0.2.10",
     author="Tom Leach",
     author_email="tom@gc.io",
     description="Powerful schema-based validation of Python dicts",
